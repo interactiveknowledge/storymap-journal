d79f2722a0ffc4cb23bdabaf7b481a556fd2222e
Philip Curley <philip@interactiveknowledge.com>
2019-10-11 14:20:38 -0400

    LG-233: Fix active screen background render

--- src/app/wrapper/common/states/Active.js
+++ src/app/wrapper/common/states/Active.js
@@ -1,34 +1,15 @@
 define([
-  'lib-build/tpl!../../tpl/sections/Interaction/Active',
-  'lib-build/tpl!../../tpl/sections/Info/Active'
+  'wrapper/utils/layout'
 ], function (
-  activeInteractionTpl,
-  activeInfoTpl
+  layout
 ) {
   return function Active () {
-
-    /**
-     * Set up the default view on the attract screen
-     */
-    var init = function () {
-      console.log('wrapper.tpl.llc.Active -- init');
-
-      $('.interaction__active').html(activeInteractionTpl());
-      $('.info__active').html(activeInfoTpl(ik.wrapper.layout.state.active.section.info))
-
-      $('#explore-1').click(function (e) {
-        e.preventDefault();
-
-        reset('602866fef9d14b20bd75b83d94fd6bca');
-      })
-    }
-
     /**
      * Show active elements
      * Hide all others
      */
     var show = function () {
-      $('.fullscreen-bg').css('background-image', 'url(' + ik.wrapper.layout.state.active.background.img + ')');
+      layout.setBackground();
 
       $('#menu').children().hide();
       $('.menu__active').show();
