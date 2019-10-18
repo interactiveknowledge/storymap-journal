65c13520196839e2d34c2956c68eaecba250f1c8
Philip Curley <philip@interactiveknowledge.com>
2019-10-18 08:25:46 -0400

    LG-247

--- src/app/wrapper/common/sections/Info.js
+++ src/app/wrapper/common/sections/Info.js
@@ -52,10 +52,10 @@ define([
 
       if (currentLanguage === 'en') {
         renderData.h1 = 'Why LEAF Matters';
-        renderData.desc = '';
+        renderData.desc = 'Won\'t stars face given living you\'re wherein shall dry you unto so grass may seed divided after brought. Fill isn\'t called, divided that. Two morning had herb creature two. Very hath dominion fly she\'d herb. Them you grass. Air it beginning first saying divided he abundantly be in over fill.';
       } else {
         renderData.h1 = 'Por qué LEAF es importante';
-        renderData.desc = '';
+        renderData.desc = 'Embarcaronse por fin a 5 de Diciembre de 1745, y el lunes 6 a las diez horas de dia, habiendo disparado la pieza de leva, se hicieron a la vela en nombre de Dios, con viento fresco, y salieron a ponerse en franquia en el amarradero, que dista tres leguas de Buenos Aires.';
       }
 
       $('.info__active').html(infoActiveTpl(renderData));
